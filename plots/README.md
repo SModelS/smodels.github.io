8afee34f0f42552d86a0a4068ba6f307087304d2
Wolfgang Waltenberger <wolfgang.waltenberger@gmail.com>
2020-07-02 13:33:32 +0200

    plots

---
+++ plots/README.md
@@ -0,0 +1,5 @@
+# collection of plots
+<img src=./origS100.png>
+<img src=./HorigS100.png>
+<img src=./S100.png>
+<img src=./HS100.png>
---
+++ plots/mkPage.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+import glob
+
+def main():
+    with open("README.md","wt") as f:
+        f.write ( "# collection of plots\n" )
+        files = glob.glob("./*" )
+        for F in files:
+            if F.endswith ( ".py" ) or F.endswith ( ".md" ):
+                continue
+            f.write ( f"<img src={F}>\n" )
+        
+
+main()
