773f635a9f809e7f13a74b3fd5974995c958e147
walten <wolfgang.waltenberger@gmail.com>
2021-03-18 17:47:24 +0100

    script added

---
+++ database/create.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+
+def sizeof_fmt(num, suffix='B'):
+    for unit in ['','Ki','Mi','Gi','Ti','Pi','Ei','Zi']:
+        if abs(num) < 1024.0:
+            return "%3.1f%s%s" % (num, unit, suffix)
+        num /= 1024.0
+    return "%.1f%s%s" % (num, 'Yi', suffix)
+
+import glob, os, pickle, time, sys, json
+files = glob.glob("*" )
+
+f=open("README.md","w")
+f.write ( "# SModelS Databases\n\n" )
+f.write ( "|        **label**        |         **mtime**         | **size** |\n" )
+f.write ( "|-------------------------|---------------------------|----------|\n" )
+for F in files:
+    if F.endswith(".py") or F.endswith(".md") or F.endswith(".html"):
+        continue
+    print ( "open",F )
+    with open(F,"rt") as g:
+        d = json.load( g )
+        mtime = "???"
+        if "mtime" in d:
+            mtime = d["mtime" ]
+        size = sizeof_fmt ( d["size"] )
+        line = f'| {F:23.23} | {mtime:25.25} | {size:8.8} |\n'
+        f.write ( line )
+
+f.close()
