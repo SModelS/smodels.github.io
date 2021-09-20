1f2091e772128755ce62bbbaeeb24d2d48a9d0f5
walten <wolfgang.waltenberger@gmail.com>
2021-09-20 16:34:00 +0200

    ratio plot

--- plots/run.sh
+++ plots/run.sh
@@ -13,6 +13,8 @@
 
 ../../smodels-utils/covariances/plotBestSRs.py -a ATLAS-SUSY-2013-09-eff -v "T6ttWW_2EqMassAx_EqMassB2y_EqMassCy.py,T6ttWWoff_2EqMassAx_EqMassB2y_EqMassCy.py"
 
+../../smodels-utils/covariances/plotRatio.py -a1 ATLAS-SUSY-2013-09 -a2 ATLAS-SUSY-2013-09-eff -v1 "T6ttWW_2EqMassAx_EqMassB2y_EqMassCy.py,T6ttWWoff_2EqMassAx_EqMassB2y_EqMassCy.py" -v2 "T6ttWW_2EqMassAx_EqMassB2y_EqMassCy.py,T6ttWWoff_2EqMassAx_EqMassB2y_EqMassCy.py" -Z 1.5 -z .1 -l1 'UL' -l2 'eff' -s
+
 ../../smodels-utils/covariances/plotBestSRs.py -a ATLAS-SUSY-2013-04-eff -v "T5WW_2EqMassAx_EqMassB0.5x+0.5y_EqMassCy.py,T5WWoff_2EqMassAx_EqMassB0.5x+0.5y_EqMassCy.py"
 
 #../../smodels-utils/covariances/plotBestSRs.py -a ATLAS-SUSY-2018-06-eff -v "TChiWZ_2EqMassAx_EqMassBy.py,TChiWZoff_2EqMassAx_EqMassBy.py"
