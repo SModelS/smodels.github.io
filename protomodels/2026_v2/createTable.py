843bd681be46d79afb5aeb11ee58f46a2ecddea2
Wolfgang Waltenberger <wolfgang.waltenberger@gmail.com>
2026-03-02 09:53:41 +0100

    x

--- protomodels/2026_v2/createTable.py
+++ protomodels/2026_v2/createTable.py
@@ -3,6 +3,7 @@
 """ create the html table that lists all subdirs """
 
 import glob, os
+from typing import Union
 
 def header ( f ):
     f.write ( "<table>\n" )
@@ -12,22 +13,49 @@ def header ( f ):
     f.write ( "    <th scope='col'>Name</th>\n" )
     f.write ( "    <th scope='col'>Link</th>\n" )
     f.write ( "    <th scope='col'>Description</th>\n" )
+    f.write ( "    <th scope='col'>K</th>\n" )
+    f.write ( "    <th scope='col'>TL</th>\n" )
     f.write ( "  </tr>\n" )
     f.write ( "</thead>\n" )
 
 def getContent ( subdir : os.PathLike, what : str = "explanation" ):
     explfile = f"{subdir}/{what}"
     if not os.path.exists ( explfile ):
-        return "no explanation found"
+        return f"no {what} found"
     with open  ( explfile, "rt" ) as f:
         return f.read()
 
-def body ( f, globpattern : str ):
+def getTestStat ( webpage : str, teststat : str ) -> str:
+    """ scrape the test statistic 'teststat' off the webpage """
+    if not os.path.exists ( webpage ):
+        return "n/a"
+    with open ( webpage, "rt" ) as f:
+        lines = f.readlines()
+        for line in lines:
+            if not "Current best" in line:
+                continue
+            p1 = line.find(f"<i>{teststat}</i>=")
+            half = line[p1+8:p1+22]
+            p1 = half.find("=")
+            half = half[p1+1:]
+            p2 = half.find(",")
+            if p2 > 0:
+                half = half[:p2]
+            p2 = half.find("<")
+            if p2 > 0:
+                half = half[:p2]
+            return half
+    return "n/a"
+
+
+def body ( f, globpattern : Union[list,str] ):
     """ the body of index.html
     :param globpattern:  e.g. 'fake*'
     """
     f.write ( "<tbody>\n" )
-    files = glob.glob ( f"./{globpattern}" )
+    files = globpattern
+    if type ( globpattern ) == str:
+        files = glob.glob ( f"./{globpattern}" )
     for subdir in files:
         if subdir.endswith(".html"):
             continue
@@ -35,11 +63,16 @@ def body ( f, globpattern : str ):
             continue
         expl = getContent ( subdir, "explanation" )
         label = getContent ( subdir, "name" )
-        link = f'<a href="{subdir}/index.html" target="_top">link</a>'
+        indexpage = f"{subdir}/index.html"
+        link = f'<a href="{indexpage}" target="_top">link</a>'
         f.write ( "  <tr>\n" )
         f.write ( f"    <td>{label}</td>\n" )
         f.write ( f"    <td>{link}</td>\n" )
         f.write ( f"    <td>{expl}</td>\n" )
+        K = getTestStat ( indexpage, "K" )
+        TL = getTestStat ( indexpage, "TL" )
+        f.write ( f"    <td>{K}</td>\n" )
+        f.write ( f"    <td>{TL}</td>\n" )
         f.write ( "  </tr>\n" )
 
     f.write ( "</tbody>\n" )
@@ -54,5 +87,6 @@ def create( globpattern : str, outfile : str ):
         footer ( f )
 
 if __name__ == "__main__":
-    create( "hiscore*", "hiscore.html" )
-    create( "fake*", "closure.html" )
+    create( [ "hiscore_20000", "hiscore", "hiscore_stops" ], "hiscore.html", )
+    create( [ "fake_stops3", "fake_ewkoff3", "fake_ewk2" ], "closure.html" )
+    create( "noX2Z*", "noX2Z.html" )
